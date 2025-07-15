ac1ebeb85cfa3aceab2b7a27ac75b86818a623b2
Oleg <zizu19988@gmail.com>
2025-07-15 15:53:51 +0300

    :sparkles: [develop] Rework hero logo animation

--- components/The/TheHeader.vue
+++ components/The/TheHeader.vue
@@ -1,8 +1,8 @@
 <template>
-    <header class="the-header container" :class="{ 'is-collapsed': scrollY > 100, 'is-dark': mainStore.isHeaderDark }">
+    <header class="the-header container" :class="{ 'is-collapsed': scrollY > 0, 'is-dark': mainStore.isHeaderDark }">
         <div class="the-header__content">
             <!-- Description -->
-            <p class="the-header__desc">A subtle concept for game design or interior mood</p>
+            <p ref="desc" class="the-header__desc">A subtle concept for game design or interior mood</p>
 
             <!-- Logo-->
             <a ref="logo" class="the-header__logo" href="#hero">
@@ -33,15 +33,43 @@
 
     // Refs
     const $refLogo = useTemplateRef("logo");
+    const $refDesc = useTemplateRef("desc");
 
     // Window
     const { y: scrollY } = useWindowScroll();
     const { width: windowWidth } = useWindowSize();
 
-    // Calculate logo scale based on window width
-    const logoScale = computed(() => {
-        if (!$refLogo?.value) return 1;
-        return 0.075 + Math.max(0, 1440 - windowWidth.value) / 5500;
+    // Animation
+    useAnimation({
+        onEnter: ({ $gsap, transitions }) => {
+            const logoScrub = $gsap.timeline({
+                scrollTrigger: {
+                    start: 0,
+                    end: "+=100%",
+                    scrub: true
+                },
+                defaults: {
+                    ease: "none"
+                }
+            });
+
+            logoScrub.to(
+                $refLogo.value,
+                {
+                    scale: () => 0.075 + Math.max(0, 1440 - windowWidth.value) / 5500,
+                    y: "1%"
+                },
+                0
+            );
+
+            logoScrub.to(
+                $refDesc.value,
+                {
+                    y: "-20vh"
+                },
+                0
+            );
+        }
     });
 </script>
 
@@ -92,24 +120,11 @@
     }
 
     // Collapsed state
-    .the-header {
-        &__desc,
-        &__logo {
-            transition: transform 1.5s $tr-atf;
-        }
-    }
-
     .the-header.is-collapsed {
         z-index: 1001;
 
-        .the-header__desc {
-            transform: translateY(-20vh);
-        }
-
         .the-header__logo {
-            --scale: v-bind(logoScale);
             pointer-events: auto;
-            transform: translateY(1%) scale(var(--scale));
         }
     }
 
