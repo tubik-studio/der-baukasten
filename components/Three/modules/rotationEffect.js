695bfe64743d757a8cde71ee0adf4252810447ad
Oleg <zizu19988@gmail.com>
2025-07-22 10:28:43 +0300

    :sparkles: [develop] Add rotation effect to the footer objects

--- components/Three/modules/rotationEffect.js
+++ components/Three/modules/rotationEffect.js
@@ -5,14 +5,27 @@ function lerp(start, end, alpha) {
 }
 
 export function updateRotationEffect(strength, lerpFactor = 0.025) {
-    if (globals.cursorPosition && globals.cameraGroup) {
-        // globals.cameraGroup.rotation.x = globals.cursorPosition.y * strength;
-        // globals.cameraGroup.rotation.y = globals.cursorPosition.x * strength;
+    if (globals.cursorPosition && globals.cameraGroup && globals.currentLoadedModel) {
+        const main = globals?.currentLoadedModel?.getObjectByName("Main_Bricks");
+        const rooster = globals?.currentLoadedModel?.getObjectByName("Mega_Rooster");
+        const storky = globals?.currentLoadedModel?.getObjectByName("Mega_Storky");
 
         const targetX = globals.cursorPosition.y * strength;
         const targetY = globals.cursorPosition.x * strength;
 
-        globals.cameraGroup.rotation.x = lerp(globals.cameraGroup.rotation.x, targetX, lerpFactor);
-        globals.cameraGroup.rotation.y = lerp(globals.cameraGroup.rotation.y, targetY, lerpFactor);
+        if (main) {
+            main.rotation.x = lerp(main.rotation.x, targetX, lerpFactor);
+            main.rotation.y = lerp(main.rotation.y, targetY, lerpFactor);
+        }
+
+        if (rooster) {
+            rooster.rotation.x = lerp(rooster.rotation.x, targetX, lerpFactor);
+            rooster.rotation.y = lerp(rooster.rotation.y, targetY, lerpFactor);
+        }
+
+        if (storky) {
+            storky.rotation.x = lerp(storky.rotation.x, targetX, lerpFactor);
+            storky.rotation.y = lerp(storky.rotation.y, targetY, lerpFactor);
+        }
     }
 }
