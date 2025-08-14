85ba0c352f8e9fafab4ad3f44f88fae022c4ab8b
Oleg <zizu19988@gmail.com>
2025-08-14 10:35:09 +0300

    :sparkles: [develop] Add animations to the mobile nav

--- components/Cards/CardsMobileNavItem.vue
+++ components/Cards/CardsMobileNavItem.vue
@@ -16,16 +16,35 @@
         onEnterDone: ({ $gsap, $scrollTrigger }) => {
             $scrollTrigger.create({
                 start: () => window.innerHeight + props.id * window.innerHeight * 2 + window.innerHeight * 0.25 + 28,
-                markers: true,
                 onEnter: () => {
+                    // Hide the current item
                     $gsap.set($refItem.value, {
                         alpha: 0
                     });
+
+                    // Make the next item bigger
+                    const nextItem = document.querySelector(`.cards-mobile-nav__item:nth-child(${props.id + 2})`);
+
+                    if (nextItem) {
+                        $gsap.set(nextItem, {
+                            scaleX: props.id + 2
+                        });
+                    }
                 },
                 onLeaveBack: () => {
+                    // Show the current item
                     $gsap.set($refItem.value, {
                         alpha: 1
                     });
+
+                    // Make the next item smaller
+                    const nextItem = document.querySelector(`.cards-mobile-nav__item:nth-child(${props.id + 2})`);
+
+                    if (nextItem) {
+                        $gsap.set(`.cards-mobile-nav__item:nth-child(${props.id + 2})`, {
+                            scaleX: 1
+                        });
+                    }
                 }
             });
         }
@@ -34,8 +53,8 @@
 
 <style lang="scss" scoped>
     .cards-mobile-nav__item {
-        &__item {
-            height: 100%;
-        }
+        height: 100%;
+        transform-origin: right;
+        transition: transform 1s $tr-atf;
     }
 </style>
